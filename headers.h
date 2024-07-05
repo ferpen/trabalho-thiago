c35112f118695745695d5798a9b413f989973216
ferpen <72898987+ferpen@users.noreply.github.com>
2024-07-05 18:44:11 -0300

    Add files via upload

--- headers.h
+++ headers.h
@@ -11,7 +11,7 @@ typedef struct noh{
     lista *lista;
     struct noh *esquerda;
     struct noh *direita;
-    int peso;
+    short int peso;
 }noh;
 
 typedef struct arvore{
@@ -40,20 +40,22 @@ lista *crialista(char *inserida){
     }
     return list;
 }
-void inserelista(lista *list,char *inserida){
+int inserelista(lista *list,char *inserida){
     while (list->proximo!=NULL){
         list=list->proximo;          //chega no último elemento alocado da lista, portanto essa casa ainda possui valor
     }
-    list->proximo=malloc(sizeof(list));   //aloca memória pra próxima casa para guardar os dados
-    list->proximo->proximo=NULL;          //seta o ponteiro do recém criado para NULL pra não ter lixo de memória
+    list->proximo=malloc(sizeof(lista));   //aloca memória pra próxima casa para guardar os dados
+    list=list->proximo;
+    list->proximo=NULL;          //seta o ponteiro do recém criado para NULL pra não ter lixo de memória
      for (int i=0;i<50;i++){
-        list->proximo->palavra[i]='\0';  //inicializando a string da nova casa da lista, se não fizer fica lixo de memória;
+        list->palavra[i]='\0';  //inicializando a string da nova casa da lista, se não fizer fica lixo de memória;
     }
     int i=0;
     while (inserida[i]!='\0'){
-        list->proximo->palavra[i]=inserida[i];      //copia a palavra do ponteiro pra lista;
+        list->palavra[i]=inserida[i];      //copia a palavra do ponteiro pra lista;
         i++;
     }
+    return 1;
 }
 //potenciação
 int potencia(int x){
@@ -70,25 +72,48 @@ int potencia(int x){
 }
 
 noh *pegaraiz(arvore *arv){ // função para pegar a raiz da árvore, evita ter que passar a raiz como parametro;
+
 if (arv!=NULL)
     return arv->raiz;
 else
     return NULL;
 }
-void rebalancear(arvore *arv){
 
+void rebalancear(noh *no){
+    printf("chave %d peso %d\n",no->chave,no->peso);
+}
+
+int confere_balanceamento(noh *no,short int *st){
+    if(no != NULL){
+    int altura[2];
+    altura[0]=confere_balanceamento(no->esquerda,st);
+    altura[1]=confere_balanceamento(no->direita,st);
+    if(*st == 0){
+    no->peso=altura[1]-altura[0];
+        if (no->peso>1||no->peso<-1){
+            rebalancear(no);
+            printf("%d\n",*st);
+            *st++;
+        }
+    }
+    return (altura[0]>altura[1]) ? altura[0]+1:altura[1]+1;
+    }
+    else{
+        return 0;
+    }  
 }
 
 int insert (arvore *arv,int key,char *palavra){  // nome auto explicativo;
  noh *raiz=pegaraiz(arv),*pai=NULL;
+
     if(raiz!=NULL){
         //encontra o lugar na árvore e salva o nó pai;
         while(raiz!=NULL){
             if (raiz->chave==key){
                 inserelista(raiz->lista,palavra);
-                break;
+                return 1;
             }
-            if (raiz->chave<key){
+            else if (raiz->chave<key){
                 pai=raiz;
                 raiz=raiz->direita;
             }
@@ -111,12 +136,21 @@ int insert (arvore *arv,int key,char *palavra){  // nome auto explicativo;
         else{
             pai->esquerda=raiz;
         }
+        arv->tamanho++;
         }
         return 1;
     }
     else{
-        return -1;
+        raiz=malloc(sizeof(noh));
+        arv->raiz=raiz;
+        raiz->chave = key;
+        raiz->lista = crialista(palavra);
+        raiz->peso = 0;
+        raiz->direita=NULL;
+        raiz->esquerda = NULL;
+        arv->tamanho++;
     }
+    return 1;
 }
 
 // pega as palavras do arquivo e armazena na estrutura (sem defeitos no momento)
@@ -125,15 +159,15 @@ int init(FILE *arquivo, arvore *arv){
     //declaração de variáveis locais;
     char a[50],*palavra;
     int i=0,key=0,letra;
-    short int letras[26];
+    short int letras[26],st;
     while((palavra = fgets(a,50,arquivo)) != NULL){  // esse loop le as palavras do arquivo até chegar ao fim dele;
-
         //reinicia as váriaveis para a proxima palavra;
         for (i=0;i<26;i++){
             letras[i]=0;
         }
         key=0;
         i=0;
+        st=0;
 
         while (a[i] != '\n'){                       //esse loop cria uma chave, somando os valores das letras da palavra e armazenando em "key";
             letra = ((int)a[i]-97);
@@ -144,8 +178,9 @@ int init(FILE *arquivo, arvore *arv){
             i++;
         }
         //insere na árvore os valores;
-        int t = insert(arv,key,palavra);
-        printf("%d/",t);
+        insert(arv,key,palavra);
+        printf("recomeco\n");
+        confere_balanceamento(arv->raiz,&st);
     }
     return 1;
     }
@@ -153,4 +188,16 @@ int init(FILE *arquivo, arvore *arv){
     else{
         return -1;
     }
+}
+//parte interativa com o usuário
+int menu(){
+
+}
+
+//parte do acesso aos valores da árvore
+int criarchave(){
+
+}
+int listarpalavras(int key,arvore *arv){
+
 }
--- trabalho_titi.c
+++ trabalho_titi.c
@@ -1,11 +1,13 @@
 #include "headers.h"
-
-//a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
 int main (void){
     FILE *arquivo = fopen("palavras.txt","r");
     arvore *arv;
+    short int b=0;
     arv = criaarvore();
-    int x = init(arquivo,arv);
-    printf("%d",x);
+    init(arquivo,arv);
+
+   // while (b!=2){
+   // b=menu();
+   // }
     return 0;
 }
